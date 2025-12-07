6798f22491b0a03f42b870c1aaf7e95b860a7f79
Daljeet Singh <daljeetsingh-@outlook.com>
2025-12-07 17:25:34 +0530

    url mismatch done

---
+++ docs/_app/immutable/entry/start.BV3eSY2C.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DPYGvJCd.js";import{x as t}from"../chunks/CHXWUN1W.js";export{t as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: ''
+      base: ""
     },
     prerender: {
       handleHttpError: 'warn'
