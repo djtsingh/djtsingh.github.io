ec9ce2a0bc5b66f5891035e9977d69ff235d56b5
Daljeet Singh <daljeetsingh-@outlook.com>
2025-12-10 12:55:35 +0530

    Add 404 error page component with suggestions and navigation links

--- docs/_app/immutable/entry/start.D2_G3k3w.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DqNKUS1H.js";import{x as t}from"../chunks/DXy1P6pw.js";export{t as load_css,r as start};
---
+++ docs/_app/immutable/entry/start.mFsEzNyd.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BBk2_zPW.js";import{x as t}from"../chunks/bpLn0vx2.js";export{t as load_css,r as start};
