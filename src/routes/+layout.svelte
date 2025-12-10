91b15c1ab1a5a849786f9ddc2b77ea9447c650b6
Daljeet Singh <daljeetsingh-@outlook.com>
2025-12-10 12:43:09 +0530

    add trackers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,15 @@
 
 <SEO />
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-84DNZR8VET"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-84DNZR8VET');
+</script>
+
 <div class="app">
   <Header />
   
