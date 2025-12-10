68002660b89cdd4dcbef60c0867fc4b2a7699732
Daljeet Singh <daljeetsingh-@outlook.com>
2025-12-10 12:47:09 +0530

    add trackers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,16 +9,18 @@
   $: pageKey = $page.url.pathname;
 </script>
 
-<SEO />
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-84DNZR8VET"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-84DNZR8VET');
+  </script>
+</svelte:head>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-84DNZR8VET"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-84DNZR8VET');
-</script>
+<SEO />
 
 <div class="app">
   <Header />
